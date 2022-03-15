7c77bce662d0f0b55676c6c457b885085af3be03
SodiqRabiu53 <olowonilara53@gmail.com>
2022-03-15 11:06:25 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,34 @@
+
+    let menu = document.querySelector('#menu-btn');
+    let header = document.querySelector('.header');
+
+     menu.onclick = () =>{
+        menu.classList.toggle('fa-times');
+        header.classList.toggle('active');
+
+    
+    }
+
+   
+    let themeToggler = document.querySelector('#theme-toggler');
+
+    themeToggler.onclick = () =>{
+        themeToggler.classList.toggle('fa-sun-o');
+        if (themeToggler.classList.contains('fa-sun-o')){
+             document.body.classList.add('active');
+        }else{
+        document.body.classList.remove('active');
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
